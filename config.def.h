e55186b03f911f302fe53cf6f599dec03b82d8af
Eli Céspedes <eli.cespedes@protonmail.com>
2022-11-07 14:11:37 -0300

    actualfullscreen

--- config.def.h
+++ config.def.h
@@ -82,6 +82,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,             		XK_c,      killclient,     {0} },
 	{ MODKEY,             		XK_space,  togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
