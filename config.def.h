95022078bba17da10c0169e94679b9372832be54
Eli Céspedes <eli.cespedes@protonmail.com>
2022-11-07 12:31:43 -0300

    restoreafterreset

--- config.def.h
+++ config.def.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#define SESSION_FILE "/tmp/dwm-session"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
